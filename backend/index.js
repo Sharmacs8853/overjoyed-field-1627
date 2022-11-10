cb1c492db087e140963590fa691cbf40c4cae932
Abiramirajasekar <abiramirajasekar002@gmail.com>
2022-11-10 17:53:48 +0530

    admin_signup/login

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,7 @@ const cors = require("cors");
 // const { default: mongoose } = require("mongoose");
 const { jobModel } = require("./Models/Job.model");
 const { adminModel } = require("./Models/Admin.model");
+const { adminController } = require("./Routes/Admin.route");
 app.use(cors())
 app.use(express.json());
 
@@ -20,67 +21,12 @@ app.get("/", (req, res) => {
 });
 
 app.use("/user", userController);
+app.use("/admin", adminController)
 
 app.get("/job", async (req, res) => {
   const job = await jobModel.find()
   // console.log(job)
   res.send(job)
-})
-
-app.post("/adminsignup", async (req, res) => {
-  const { email, password ,name} = req.body;
-  const existing_user = await adminModel.findOne({ email });
-
-  if (existing_user) {
-    res.send("Admin already exist")
-    return;
-  }
-  bcrypt.hash(password, 4, async function (err, hash) {
-    if (err) {
-      res.send({"msg":"signup failed ..please try again.."});
-    } else {
-      
-      const new_admin = new adminModel({
-        email,
-        password: hash,
-        name,
-       
-      });
-
-      await new_admin.save();
-      res.send({msg:"signup succesfull.."});
-    }
-  });
-});
-
-
-app.post("/adminlogin", async (req, res) => {
-  const {email, password} = req.body
-
-  const admin = await adminModel.findOne({email})
-
-  const hashed_password = admin.password;
-
-  const admin_id = admin._id;
- 
-  if(admin){
-
-    bcrypt.compare(password, hashed_password, function(err, result) {
-      if(err){
-        res.send({"msg" : "Something went wrong, try again later"})
-      }
-      if(result){
-        const token = jwt.sign({admin_id}, process.env.SECRET);  
-        res.send({message : "Login successfull", token})
-      }
-      else{
-        alert("login failed")
-        res.send({"msg" : "Login failed"})
-      }
-    });
-  }else{
-    res.send({msg:"only admin can access ..please login with correct credentials.."})
-  }
 })
 
 
