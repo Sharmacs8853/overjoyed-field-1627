8023f7a3869efa81f202a9d48345bd053ac78c0b
Abiramirajasekar <abiramirajasekar002@gmail.com>
2022-11-10 17:25:12 +0530

    job API backend by Abirami

--- backend/Models/Admin.model.js
+++ backend/Models/Admin.model.js
@@ -1,15 +1,14 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
+const adminSchema = mongoose.Schema({
     name:{type:String, required:true},
     email:{type:String , required:true},
     password:{type:String , required :true,min:4},
-    mobile:{type:Number, required:true },
-    work_status:{type:String,required:true ,default:"fresher"}
+    
 }) 
 
-const userModel = mongoose.model("user" , userSchema);
+const adminModel = mongoose.model("admin" , adminSchema);
 
 module.exports={
-    userModel
+    adminModel
 }
