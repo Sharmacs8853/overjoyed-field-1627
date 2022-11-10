8eef9c0c56d55bf2389d67f9ecc9d31d494c7954
sharmacs8853 <sharmacs8853@gmail.com>
2022-11-10 07:21:48 +0530

    navbar done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Box } from '@chakra-ui/react';
+import Navbar from './Components/Navbar/Navbar.jsx'
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Box>
+      <Navbar />
+    </Box>
   );
 }
 
