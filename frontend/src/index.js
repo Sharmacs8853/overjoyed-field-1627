e306ab028c0f742e9f9d0de14f72b28ebc9b1fa0
Harsha-C2508 <harshac2508@gmail.com>
2022-11-11 10:27:14 +0530

    branch 2

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-<<<<<<< HEAD
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-=======
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// =======
 //import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
@@ -25,8 +25,6 @@ root.render(
     </ChakraProvider>
     </Provider>
 </BrowserRouter>
-    
->>>>>>> 4a38572e451acdec22deed38c700214bdfd3b502
 );
 
 // If you want to start measuring performance in your app, pass a function
