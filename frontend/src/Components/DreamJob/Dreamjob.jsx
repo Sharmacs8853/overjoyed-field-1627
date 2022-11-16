f80ae76724bacdba09a9f0767f3d4ff9a25fa41e
Ritesh134340 <riteshkumar134340@gmail.com>
2022-11-16 09:40:15 +0530

    added search functionality

--- frontend/src/Components/DreamJob/Dreamjob.jsx
+++ frontend/src/Components/DreamJob/Dreamjob.jsx
@@ -1,8 +1,49 @@
-import { Box, Button, Flex, Heading, Input, Spacer, Text } from '@chakra-ui/react'
+import { Box, Button, Flex, Heading, Input, Spacer, Text,Img } from '@chakra-ui/react'
 import React from 'react'
+import {Link} from "react-router-dom"
+import { BsFilter, BsStarFill, BsFileEarmarkText} from 'react-icons/bs'
+import { IoLocationOutline } from "react-icons/io5";
 import './Dreamjob.css'
+import { useEffect,useState } from 'react'
+import axios from "axios"
 
 const Dreamjob = () => {
+    const [job ,setJobs] =useState([]);
+    const {REACT_APP_MONGO_URL}=process.env;
+    const [filterData,setFilterData]=useState([])
+    const [show,setShow]=useState(false)
+    const [val,setValue]=useState("")
+  
+
+  useEffect(()=>{
+  axios.get(`${REACT_APP_MONGO_URL}/job`).then((res)=>{
+    setJobs(res.data)
+  }).catch((err)=>{
+    console.log(err)
+  })
+  },[])
+ 
+ const handleFilter=(e)=>{
+    setValue(e.target.value)
+    const filterResult=job && job.filter(item=>item.job_title.toLowerCase().includes(e.target.value.toLowerCase()))
+    setFilterData(filterResult)
+   
+ }
+ useEffect(()=>{
+    if(val===""){
+        setShow(false)
+     }
+     if(val!==""){
+        setShow(true)
+     }
+ },[val])
+ 
+
+ 
+console.log(job)
+
+
+
     return (
         <Box className='job-div'>
             <Box className='inner-job-div'>
@@ -15,7 +56,7 @@ const Dreamjob = () => {
                                 <i  className="fa-solid fa-magnifying-glass"></i>
                             </Box>
                             <Box display={"flex"} align-items={"center"} borderLeft={'1px solid'} borderColor={'gray.300'}>
-                                <Input variant='unstyled' width={'300px'} px={'10px'} placeholder='Enter skills/ designations / Companies' type='text' />
+                                <Input variant='unstyled' width={'300px'} px={'10px'} placeholder='Enter skills/ designations / Companies' type='text' onChange={handleFilter}/>
                             </Box>
                             <Box display={"flex"} align-items={"center"} borderLeft={'1px solid'} borderColor={'gray.300'}>
                                 <Input variant='unstyled' px={'10px'} placeholder='Select experience' type='text' />
@@ -32,7 +73,25 @@ const Dreamjob = () => {
                 </Box>
             </Box>
 
-        </Box>
+            {/* from here I have  */}
+
+{show && <Box className="filter-by-search">
+
+ {filterData && filterData.map((item)=>
+     
+        <Box className="filter-indv-div">
+            <Link to={`/jd/${item._id}`} className="filter-link">
+                <div className="filter-left-div">
+                <h1 className="head-of-job">{item.job_title}</h1>
+                 <h5 className="dis-of-comp">{item.company_description}</h5>  
+                </div>
+                <img src={item.url} className="filter-img"></img>
+            </Link>
+        </Box>   
+ )}
+</Box>}
+</Box>
+        
     )
 }
 
